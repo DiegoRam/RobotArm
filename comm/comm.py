5a3489d26891faeacbff14d2530f0bf32a270a56
diegoram <diego@diegoram.me>
2017-03-31 10:08:58 -0300

    fixes

--- comm/comm.py
+++ comm/comm.py
@@ -1,9 +1,9 @@
-import socket,configparser, logging, sys, getopt
+import socket,configparser, logging, sys, getopt, thread
 from time import sleep
 import tornado
 import tornado.websocket
 import tornado.httpserver
-from websocket import create_connection
+from websocket import WebSocketApp
 
 
 config = configparser.ConfigParser()
@@ -21,24 +21,27 @@ handler = logging.StreamHandler()
 file_handler = logging.FileHandler('/var/tmp/socket_server.log')
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
-if env == 'prod':
+if env == 'pi':
 	file_handler.setFormatter(formatter)
 	log.addHandler(file_handler)
 log.addHandler(handler)
 
-if env == "prod":
+if env == 'pi':
 	from gpiozero import AngularServo
+elif env == 'mac':
+    pass
 
 class Arm(object):
     def __init__(self, pinsTuple):
         self._pin1, self._pin2, self._pin3, self._pin4 = pinsTuple
-        self._servo_base = AngularServo(self._pin1, min_angle=-80, max_angle = 80)
-        self._servo_arm1 = AngularServo(self._pin2, min_angle=-80, max_angle = 80)
-        self._servo_arm2 = AngularServo(self._pin3, min_angle=-80, max_angle = 80)
-        self._servo_claw = AngularServo(self._pin4, min_angle=-80, max_angle = 80)
+        if env == 'pi':
+            self._servo_base = AngularServo(self._pin1, min_angle=-80, max_angle = 80)
+            self._servo_arm1 = AngularServo(self._pin2, min_angle=-80, max_angle = 80)
+            self._servo_arm2 = AngularServo(self._pin3, min_angle=-80, max_angle = 80)
+            self._servo_claw = AngularServo(self._pin4, min_angle=-80, max_angle = 80)
 
     def center(self):
-        if env == "prod":
+        if env == 'pi':
             self._servo_base.mid()
             self._servo_arm1.mid()
             self._servo_arm2.mid()
@@ -50,28 +53,28 @@ class Arm(object):
             self._servo_claw.detach()
 
     def claw_pos(self, angle):
-        if env == "prod":
+        if env == 'pi':
             self._servo_claw.angle(angle)
             sleep(0.2)
             self._servo_claw.detach()
         log.debug("Claw servo moved to: " + angle)
 
     def base_pos(self, angle):
-        if env == "prod":
+        if env == 'pi':
             self._servo_base.angle = angle
             sleep(0.2)
             self._servo_base.detach()
         log.debug("Base servo moved to: " + angle)
 
     def arm1_pos(self, angle):
-        if env == "prod":
+        if env == 'pi':
             self._servo_arm1.angle = angle
             sleep(0.2)
             self._servo_arm1.detach()
         log.debug("Arm1 servo moved to: " + angle)
 
     def arm2_pos(self, angle):
-        if env == "prod":
+        if env == 'pi':
             self._servo_arm2.angle = angle
             sleep(0.2)
             self._servo_arm2.detach()
@@ -79,7 +82,7 @@ class Arm(object):
 
 
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
-    ARM = Arm()
+    ARM = Arm((17,11,10,9))
     def open(self):
         log.debug("WebSocket opened")
 
@@ -87,7 +90,7 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
         self.write_message(u"You said: " + message)
         log.debug(u"You said: " + message)
         #TODO logic about servo motors
-        ARM.base_pos(message)
+        self.ARM.base_pos(message)
 
     def on_close(self):
         log.debug("WebSocket closed")
@@ -97,8 +100,9 @@ class Client(object):
         self._host = host
         self._port = port
         log.debug("Establishing connection...")
-        self._ws = create_connection("ws://localhost:9005/robotarm")
+        self.ws = WebSocketApp("ws://localhost:9005/robotarm", on_close = on_close, on_message = on_message)
         log.debug("Connection established...")
+        self.ws.on_open = on_open
 
 
     def readCamera(self):
@@ -109,8 +113,20 @@ class Client(object):
 
     def send(self, message):
         log.debug("Sending message: " + message)
-        self._ws.send(message)
+        self.ws.send(message)
 
+def on_message(ws, message):
+    log.debug("Received from server: " + message)
+
+def on_close(ws):
+    log.debug("Client closed...")
+
+def on_open(ws):
+    def run(*args):
+        while True:
+            data = raw_input("Enter angle: ")
+            ws.send(data)
+    thread.start_new_thread(run,())
 
 def main(argv):
     try:
@@ -140,9 +156,7 @@ def main(argv):
 
     elif MODE == "client":
         client = Client()
-        while True:
-            data = raw_input("Insert angle :")
-            client.send(data)
+        client.ws.run_forever()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
